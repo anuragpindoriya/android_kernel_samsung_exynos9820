fdbcb6bf740436223b5d425fdd951511bc982596
Victor Bessonov <victor.a.bessonov@gmail.com>
2023-11-05 12:27:56 +0300

    Switch to LLVM build

--- build.sh
+++ build.sh
@@ -11,18 +11,14 @@ echo "Rendering the config from defconfig of the device specified..."
 make O=${OUTPUT_DIR} ARCH=arm64 exynos9820-${DEVICE}_defconfig
 
 echo "Building the kernel..."
-# Set PATH to clang and crossgcc for aarch64 and (just in case) arm architectures
-PATH="/workdir/clang/bin:/workdir/gcc-aarch64/bin:/workdir/gcc-arm/bin:${PATH}" \
+# Set PATH to clang only (Linux4 mentioned we don't need GCC anymore)
+PATH="/workdir/clang/bin:${PATH}"
 # Fix libdss-build path in Makefile so it won't be searched in OUTPUT dir
 sed -i 's|bash -C lib/libdss-build.sh|bash -C ../lib/libdss-build.sh|' Makefile
 rm -f build.log
 make -j$(nproc --all) 2>&1 O=${OUTPUT_DIR} \
                            ARCH=arm64 \
-                           CC=clang \
-                           CLANG_TRIPLE=aarch64-linux-gnu- \
-                           CROSS_COMPILE=aarch64-linux-android- | tee build.log
-
-#CROSS_COMPILE_ARM32=arm-linux-androideabi-
+                           LLVM=1 | tee build.log
 
 echo "Creating the DTB image..."
 build-tools/mkdtimg cfg_create ${OUTPUT_DIR}/dtb build-tools/exynos9820-dtb.cfg -d ${OUTPUT_DIR}/arch/arm64/boot/dts/exynos
@@ -31,3 +27,15 @@ echo "Building installable zip with AnyKernel..."
 mv ${OUTPUT_DIR}/arch/arm64/boot/Image build-tools/AnyKernel3/zImage
 mv ${OUTPUT_DIR}/dtb build-tools/AnyKernel3/dtb
 cd build-tools/AnyKernel3 && zip -r9 LOS-Kernel-Unb0rn.zip .
+
+# Cleanup
+rm -f  dtb zImage
+
+
+# This is here only for historical purposes - the kernel used to be build that way, also pathes to crossgcc should be added to PATH
+#make -j$(nproc --all) 2>&1 O=${OUTPUT_DIR} \
+#                           ARCH=arm64 \
+#                           CC=clang \
+#                           CLANG_TRIPLE=aarch64-linux-gnu- \
+#                           CROSS_COMPILE=aarch64-linux-android- \
+#                           CROSS_COMPILE_ARM32=arm-linux-androideabi-
